d3d05c26ed533bbbb4b0053445bde8e14c0d279e
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-30 09:42:21 -0700

    completed "Spatial Input with Forms" step

--- tethysdev/tethysapp-az_map/tethysapp/az_map/model.py
+++ tethysdev/tethysapp-az_map/tethysapp/az_map/model.py
@@ -4,52 +4,56 @@ import uuid
 from pathlib import Path
 
 
-def add_new_data(db_directory: Path | str, name: str, owner: str, river: str, date_built: str):
-    """
-    Persist new data.
-    """
-    # Serialize data to json
-    new_data_id = uuid.uuid4()
-    data_dict = {
-        'id': str(new_data_id),
-        'name': name,
-        'owner': owner,
-        'river': river,
-        'date_built': date_built
-    }
+def add_new_data(db_directory, location, name, owner, river, date_built):
+  """
+  Persist new data.
+  """
+  # Convert GeoJSON to Python dictionary
+  location_dict = json.loads(location)
 
-    data_json = json.dumps(data_dict)
+  # Serialize data to json
+  new_data_id = uuid.uuid4()
+  data_dict = {
+    'id': str(new_data_id),
+    'location': location_dict['geometries'][0],
+    'name': name,
+    'owner': owner,
+    'river': river,
+    'date_built': date_built
+  }
 
-    # Write to file in {{db_directory}}/datas/{{uuid}}.json
-    # Make datas dir if it doesn't exist
-    datas_dir = Path(db_directory) / 'data'
-    if not datas_dir.exists():
-        os.makedirs(datas_dir, exist_ok=True)
+  data_json = json.dumps(data_dict)
 
-    # Name of the file is its id
-    file_name = str(new_data_id) + '.json'
-    file_path = datas_dir / file_name
+  # Write to file in {{db_directory}}/datas/{{uuid}}.json
+  # Make datas dir if it doesn't exist
+  datas_dir = Path(db_directory) / 'data'
+  if not datas_dir.exists():
+    os.makedirs(datas_dir, exist_ok=True)
 
-    # Write json
-    with file_path.open('w') as f:
-        f.write(data_json)
+  # Name of the file is its id
+  file_name = str(new_data_id) + '.json'
+  file_path = datas_dir / file_name
+
+  # Write json
+  with file_path.open('w') as f:
+    f.write(data_json)
 
 def get_all_data(db_directory: Path | str):
-    """
-    Get all persisted data.
-    """
-    # Write to file in {{db_directory}}/data/{{uuid}}.json
-    # Make data dir if it doesn't exist
-    data_dir = Path(db_directory) / 'data'
-    if not data_dir.exists():
-        os.makedirs(data_dir, exist_ok=True)
+  """
+  Get all persisted data.
+  """
+  # Write to file in {{db_directory}}/data/{{uuid}}.json
+  # Make data dir if it doesn't exist
+  data_dir = Path(db_directory) / 'data'
+  if not data_dir.exists():
+    os.makedirs(data_dir, exist_ok=True)
 
-    data = []
+  data = []
 
-    # Open each json file and convert contents to python dictionaries
-    for dam_json in data_dir.glob('*.json'):
-        with dam_json.open('r') as f:
-            dam_dict = json.loads(f.read())
-            data.append(dam_dict)
+  # Open each json file and convert contents to python dictionaries
+  for data_json in data_dir.glob('*.json'):
+    with data_json.open('r') as f:
+      data_dict = json.loads(f.read())
+      data.append(data_dict)
 
-    return data
+  return data
