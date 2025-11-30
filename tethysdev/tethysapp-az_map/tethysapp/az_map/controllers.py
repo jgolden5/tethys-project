b1836ed1adfdc251c574ae8039372daba67b8fd0
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-30 13:55:36 -0700

    render spatial data on the map - big additions to code, but only in controllers.py

--- tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
+++ tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
@@ -5,13 +5,140 @@ from tethys_sdk.routing import controller
 from .app import App
 from .model import add_new_data, get_all_data
 
-@controller(name="home")
+@controller(name="home", app_workspace=True)
 class HomeMap(MapLayout):
   app = App
   base_template = f'{App.package}/base.html'
   map_title = 'AZ Water'
   map_subtitle = 'Aquaveo Project'
   basemaps = ['OpenStreetMap', 'ESRI']
+  show_properties_popup = True
+
+  def compose_layers(self, request, map_view, app_workspace, *args, **kwargs):
+    # Get list of data and create data MVLayer:
+    data = get_all_data(app_workspace.path)
+    features = []
+
+    # Define GeoJSON Features
+    for d in data:
+      data_location = d.get('location')
+      data_feature = {
+        'type': 'Feature',
+        'geometry': {
+          'type': data_location['type'],
+          'coordinates': data_location['coordinates'],
+        },
+        'properties': {
+          'id': d['id'],
+          'name': d['name'],
+          'owner': d['owner'],
+          'river': d['river'],
+          'date_built': d['date_built']
+        }
+      }
+
+      features.append(data_feature)
+
+    # Define GeoJSON FeatureCollection
+    data_feature_collection = {
+      'type': 'FeatureCollection',
+      'crs': {
+        'type': 'name',
+        'properties': {
+          'name': 'EPSG:4326'
+        }
+      },
+      'features': features
+    }
+
+    # Compute zoom extent for the data layer
+    layer_extent = self.compute_data_extent(data)
+
+    data_layer = self.build_geojson_layer(
+      geojson=data_feature_collection,
+      layer_name='data',
+      layer_title='Data',
+      layer_variable='data',
+      extent=layer_extent,
+      visible=True,
+      selectable=True,
+      plottable=True,
+    )
+
+    layer_groups = [
+      self.build_layer_group(
+        id='all-layers',
+        display_name='Layers',
+        layer_control='checkbox',
+        layers=[data_layer]
+      )
+    ]
+
+    # Update the map view with the new extent
+    map_view.view = MVView(
+      projection='EPSG:4326',
+      extent=layer_extent,
+      maxZoom=self.max_zoom,
+      minZoom=self.min_zoom,
+    )
+
+    return layer_groups
+
+  def build_map_extent_and_view(self, request, app_workspace, *args, **kwargs):
+    """
+    Builds the default MVView and BBOX extent for the map.
+
+    Returns:
+      MVView, 4-list<float>: default view and extent of the project.
+    """
+    data = get_all_data(app_workspace.path)
+    extent = self.compute_data_extent(data)
+
+    # Construct the default view
+    view = MVView(
+      projection="EPSG:4326",
+      extent=extent,
+      maxZoom=self.max_zoom,
+      minZoom=self.min_zoom,
+    )
+
+    return view, extent
+
+  def compute_data_extent(self, data):
+    """Compute the extent/bbox of the given data."""
+    lat_list = []
+    lng_list = []
+
+    # Define GeoJSON Features
+    for d in data:
+      data_location = d.get('location')
+      lat_list.append(data_location['coordinates'][1])
+      lng_list.append(data_location['coordinates'][0])
+
+    if len(lat_list) > 1:
+      # Compute the bounding box of all the data
+      min_x = min(lng_list)
+      min_y = min(lat_list)
+      max_x = max(lng_list)
+      max_y = max(lat_list)
+      x_dist = max_x - min_x
+      y_dist = max_y - min_y
+
+      # Buffer the bounding box
+      buffer_factor = 0.1
+      x_buffer = x_dist * buffer_factor
+      y_buffer = y_dist * buffer_factor
+      min_xb = min_x - x_buffer
+      min_yb = min_y - y_buffer
+      max_xb = max_x + x_buffer
+      max_yb = max_y + y_buffer
+
+      # Bounding box for the view
+      extent = [min_xb, min_yb, max_xb, max_yb]
+    else:
+      extent = [-125.771484, 24.527135, -66.005859, 49.667628]  # CONUS
+
+    return extent
 
 @controller(url='data/add', app_workspace=True)
 def add_data(request, app_workspace):
@@ -58,7 +185,7 @@ def add_data(request, app_workspace):
     if not date_built:
       has_errors = True
       date_error = 'Date Built is required.'
-
+    
     if not location:
       has_errors = True
       location_error = 'Location is required.'
@@ -108,7 +235,7 @@ def add_data(request, app_workspace):
     format='MM d, yyyy',
     start_view='decade',
     today_button=True,
-    initial='',
+    initial=date_built,
     error=date_error
   )
 
@@ -169,11 +296,11 @@ def list_data(request, app_workspace):
   data = get_all_data(app_workspace.path)
   table_rows = []
 
-  for data in data:
+  for d in data:
     table_rows.append(
       (
-        data['name'], data['owner'],
-        data['river'], data['date_built']
+        d['name'], d['owner'],
+        d['river'], d['date_built']
       )
     )
 
