07350c6feab497d60dc5f9ce2f18f5c26915a8f8
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-29 19:49:19 -0700

    add form submission handling code to controllers.py

--- tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
+++ tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from tethys_sdk.gizmos import Button, DatePicker, SelectInput, TextInput
 from tethys_sdk.layouts import MapLayout
 from tethys_sdk.routing import controller
@@ -17,10 +18,56 @@ def add_data(request):
   """
   Controller for the Add Data page.
   """
+  #Default values
+  name = ''
+  owner = 'Reclamation'
+  river = ''
+  date_built = ''
+
+  # Errors
+  name_error = ''
+  owner_error = ''
+  river_error = ''
+  date_error = ''
+
+  # Handle form submission
+  if request.POST and 'add-button' in request.POST:
+    # Get values
+    has_errors = False
+    name = request.POST.get('name', None)
+    owner = request.POST.get('owner', None)
+    river = request.POST.get('river', None)
+    date_built = request.POST.get('date-built', None)
+
+    # Validate
+    if not name:
+      has_errors = True
+      name_error = 'Name is required.'
+
+    if not owner:
+      has_errors = True
+      owner_error = 'Owner is required.'
+
+    if not river:
+      has_errors = True
+      river_error = 'River is required.'
+
+    if not date_built:
+      has_errors = True
+      date_error = 'Date Built is required.'
+
+    if not has_errors:
+      # Do stuff here
+      return App.redirect(App.reverse('home'))
+
+    messages.error(request, "Please fix errors.")
+
   #Define form gizmos
   name_input = TextInput(
     display_text='Name',
-    name='name'
+    name='name',
+    initial=name,
+    error=name_error
   )
 
   owner_input = SelectInput(
@@ -28,13 +75,16 @@ def add_data(request):
     name='owner',
     multiple=False,
     options=[('Reclamation', 'Reclamation'), ('Army Corp', 'Army Corp'), ('Other', 'Other')],
-    initial=['Reclamation']
+    initial=owner,
+    error=owner_error
   )
 
   river_input = TextInput(
     display_text='River',
     name='river',
-    placeholder='e.g.: Mississippi River'
+    placeholder='Salt River',
+    initial=river,
+    error=river_error
   )
 
   date_built = DatePicker(
@@ -44,7 +94,8 @@ def add_data(request):
     format='MM d, yyyy',
     start_view='decade',
     today_button=True,
-    initial='February 15, 2017'
+    initial=date_built,
+    error=date_error
   )
 
   add_button = Button(
@@ -52,7 +103,7 @@ def add_data(request):
     name='add-button',
     icon='plus-square',
     style='success',
-    attributes={'form': 'add-dam-form'},
+    attributes={'form': 'add-data-form'},
     submit=True
   )
 
@@ -72,3 +123,4 @@ def add_data(request):
   }
 
   return App.render(request, 'add_data.html', context)
+
