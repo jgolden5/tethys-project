cb5074eefba4e12812f922dd2b659d72866d7fc7
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-11-29 17:58:12 -0700

    add a template extending base.html and a new controller in controllers.py

--- tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
+++ tethysdev/tethysapp-az_map/tethysapp/az_map/controllers.py
@@ -8,5 +8,13 @@ class HomeMap(MapLayout):
     app = App
     base_template = f'{App.package}/base.html'
     map_title = 'AZ Water'
-    map_subtitle = 'Tutorial for Aquaveo interview'
+    map_subtitle = 'Aquaveo Project'
     basemaps = ['OpenStreetMap', 'ESRI']
+
+@controller(url='data/add')
+def add_data(request):
+    """
+    Controller for the Add Data page.
+    """
+    context = {}
+    return App.render(request, 'add_data.html', context)
